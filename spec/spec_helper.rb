a4895756d59ac6afb4a57adc777af4f1d2f2026f
Scott Hill <scott@thehills.house>
2019-01-09 16:36:35 -0600

    Add new listener startup life cycle event to allow objects to be created after listeners are forked

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,6 +33,8 @@ RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
+  config.filter_run_when_matching :focus
+
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
@@ -78,8 +80,8 @@ RSpec.configure do |config|
     topic = client.topic "test"
     if topic.nil?
       topic = client.create_topic "test"
-      subscription = topic.subscribe "test-subscriber-1"
-      subscription = topic.subscribe "test-subscriber-2"
+      subscription = topic.subscribe "test-subscription-1"
+      subscription = topic.subscribe "test-subscription-2"
     end
   end 
 
@@ -91,7 +93,7 @@ RSpec.configure do |config|
   # is tagged with `:focus`, all examples get run. RSpec also provides
   # aliases for `it`, `describe`, and `context` that include `:focus`
   # metadata: `fit`, `fdescribe` and `fcontext`, respectively.
-  config.filter_run_when_matching :focus
+  
 
   # Allows RSpec to persist some state between runs in order to support
   # the `--only-failures` and `--next-failure` CLI options. We recommend
